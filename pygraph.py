1e199ac1b02e3a9f9ca5d854b1b91d5736b5e337
gray <115596729+letlovewin@users.noreply.github.com>
2023-12-25 15:54:55 +0000

    bleh

--- pygraph.py
+++ pygraph.py
@@ -1,16 +1,10 @@
-import sys
+import sys, math, random
 from collections import deque
 
 class Graph:
-    def __init__(self,vertices=[],edges=[]):
-        self.vertices = vertices
-        self.degreeMatrix = None
-        self.laplacianMatrix = None
-        if edges==[]:
-            self.generateAdjacencyMatrix(len(vertices))
-        else:
-            self.edges = edges
-        self.completeness = (len(self.edges))==((1/2)*(len(self.edges)*(len(self.edges)-1)))
+    def __init__(self,vertices):
+        self.vertices = [i+1 for i in range(vertices)]
+        self.generateAdjacencyMatrix(vertices)
         self.edgeDirectionType()
     def edgeDirectionType(self):
         N = len(self.vertices)
@@ -23,55 +17,43 @@ class Graph:
         self.vertices = [i+1 for i in range(vertices)]
         self.edges = [[0 for _ in range(vertices)] for __ in range(vertices)]
         self.completeness = False
-        self.degreeMatrix = None
-        self.laplacianMatrix = None
-        self.edgeType = ''
+        self.edgeType = 'undirected'
     def deg(self,v):
-        if v < 0 or v >= len(self.edges):
+        if v-1 < 0 or v-1 >= len(self.edges):
             raise Exception("Provided vertex doesn't exist")
-        return len(self.edges[v])
+        k = 0
+        N = len(self.vertices)
+        for i in range(N):
+            if self.edges[v] != 0:
+                k += 1
     def createVertex(self):
         for i in self.edges:
             i.append(0)
         self.edges.append([0 for _ in range(len(self.edges)+1)])
         self.vertices.append(len(self.edges)-1)
     def addUndirectedEdge(self,u,v,weight=1):
-        if u < 0 or u > len(self.vertices):
-            for i in self.edges:
-                i.append(0)
-            self.edges.append([0 for _ in range(len(self.edges)+1)])
-            self.vertices.append(u)
-        if v < 0 or v > len(self.vertices):
-            for i in self.edges:
-                i.append(0)
-            self.edges.append([0 for _ in range(len(self.edges)+1)])
-            self.vertices.append(v)
-        #Dynamic resizing of adjacency matrix if u or v isn't in our set of vertices.
+        if u-1<0 or u-1 > len(self.edges):
+            raise Exception("Provided vertice(s) do not exist in the graph")
+        if v-1<0 or v-1 > len(self.edges):
+            raise Exception("Provided vertice(s) do not exist in the graph")
         self.edges[u-1][v-1] = weight
         self.edges[v-1][u-1] = weight
     def addDirectedEdge(self,u,v,weight=1):
+        if u-1<0 or u-1 > len(self.edges):
+            raise Exception("Provided vertice(s) do not exist in the graph")
+        if v-1<0 or v-1 > len(self.edges):
+            raise Exception("Provided vertice(s) do not exist in the graph")
         self.edgeType = 'directed'
-        if not u in self.vertices:
-            for i in self.edges:
-                i.append(0)
-            self.edges.append([0 for _ in range(len(self.edges)+1)])
-            self.vertices.append(u)
-        if not v in self.vertices:
-            for i in self.edges:
-                i.append(0)
-            self.edges.append([0 for _ in range(len(self.edges)+1)])
-            self.vertices.append(v)
-        #Dynamic resizing of adjacency matrix if u or v isn't in our set of vertices.
         self.edges[u-1][v-1] = weight
     def removeEdge(self,u,v,removeBoth=False):
         if not u in self.vertices or not v in self.vertices:
-            raise Exception("Provided vertice(s) do not exist in the graph.")
+            raise Exception("Provided vertice(s) do not exist in the graph")
         self.edges[u-1][v-1] = 0
         if removeBoth==True:
             self.edges[v-1][u-1] = 0
     def isEdge(self,u,v):
         if not u in self.vertices or not v in self.vertices:
-            raise Exception("Provided vertice(s) don't exist.")
+            raise Exception("Provided vertice(s) don't exist")
         if self.edges[u-1][v-1] != 0:
             return True
         else:
@@ -87,23 +69,60 @@ class Graph:
         if type=='bft':
             q = deque([origin])
             v = set([origin])
+            path = [origin]
             while q:
-                k = q.popleft()
+                k = q.pop()
                 for i in range(len(self.edges[k])):
                     if not i+1 in v:
                         v.add(i+1)
                         q.appendleft(i)
-            return v
+                        path.append(i+1)
+            return path
         elif type=='dft':
             q = [origin]
             v = set([origin])
+            path = [origin]
             while q:
                 k = q.pop()
                 for i in range(len(self.edges[k])):
                     if not i+1 in v:
                         v.add(i+1)
                         q.append(i)
-            return v
+                        path.append(i+1)
+            return path
+    def search(self,origin,target,type='bft'):
+        if origin-1 < 0 or origin-1 >= len(self.edges):
+            raise Exception("Provided origin vertex does not exist.")
+        if type=='bft':
+            q = deque([origin])
+            v = set([origin])
+            path = [origin]
+            while q:
+                k = q.pop()
+                for i in range(len(self.edges[k])):
+                    if not i+1 in v:
+                        v.add(i+1)
+                        q.append(i)
+                        path.add(i+1)
+                        if i+1==target:
+                            return path
+            return [-1]
+        elif type=='dft':
+            q = [origin]
+            v = set([origin])
+            path = [origin]
+            while q:
+                k = q.pop()
+                for i in range(len(self.edges[k])):
+                    if not i+1 in v:
+                        v.add(i+1)
+                        q.append(i)
+                        path.add(i+1)
+                        if i+1==target:
+                            return path
+            return [-1]
+        else:
+            raise Exception("Invalid traversal type.")
     def minDistance(self,dist,sptSet):
         min = sys.maxsize
         min_index = 0
@@ -155,20 +174,16 @@ class Graph:
         if cnt!=N:
             return([-1])
         return top_sort
-    def generateLaplacianMatrix(self):
+    def generateLaplacianMatrix(self, degreeMatrix):
         #LaplacianMatrix = degreeMatrix - adjacencyMatrix
-        if not self.degreeMatrix:
-            raise Exception("Degree matrix for this graph hasn't been initialized. Run graph.generateDegreeMatrix()")
-        if not self.edges:
-            raise Exception("Adjacency matrix for this graph hasn't been intialized. Run graph.generateAdjacencyMatrix() or add some edges.")
-        if len(self.degreeMatrix) != len(self.edges):
+        if len(degreeMatrix) != len(self.edges):
             raise Exception("Degree matrix and adjacency matrix mismatch. Check what edges and/or vertices you've added.")
         N = len(self.vertices)
         L = [[[0] for _ in range(N)] for __ in range(N)]
         for i in range(N):
             for j in range(N):
-                L[i][j] = self.degreeMatrix[i][j] - self.edges[i][j]
-        self.laplacianMatrix = L
+                L[i][j] = degreeMatrix[i][j] - self.edges[i][j]
+        return L
     def generateDegreeMatrix(self,degreeType='indegree'):
         if self.edgeType == 'undirected':
             D = []
@@ -179,7 +194,7 @@ class Graph:
                 for j in range(N):
                     if self.edges[i][j] != 0:
                         D[i][i] += 1
-            self.degreeMatrix = D
+            return D
         elif self.edgeType == 'directed':
             D = [[[0] for _ in range(len(self.vertices))] for __ in range(len(self.vertices))]
             N = len(self.vertices)
@@ -195,12 +210,30 @@ class Graph:
                             D[j][j] += 1
             else:
                 raise Exception("degreeType is invalid. Must be either 'indegree' or 'outdegree'")
-                return
-            self.degreeMatrix = D
-            return
+                return None
+            return D
         else:
-            self.edgeDirectionType() #run it again if edge direction type hasn't been updated
-            self.generateDegreeMatrix()
+            raise Exception("self.edgeType is undefined, likely because you haven't initialized an adjacency matrix.")
+    def generateRandomGraph(self,isAcyclic,isWeighted,connectionType,allowSelfPath=False,lowerBound=1,upperBound=64,disjointPossible=True):
+        if lowerBound>upperBound:
+            raise Exception("lowerBound for generation of random graph must be greater than upperBound")
+        if lowerBound<1:
+            raise Exception("lowerBound must be larger than 0")
+        V = random.randrange(lowerBound+1,upperBound)
+        A = [[0 for _ in range(V)] for __ in range(V)]
+        B = random.randrange(lowerBound,V)
+        for i in range(7):
+            y1 = random.randrange(0,V-1)
+            x1 = random.randrange(0,V-1)
+            W = random.randrange(1,512)
+            if connectionType=='undirected':
+                A[y1][x1] = W
+                A[x1][y1] = W
+            elif connectionType=='directed':
+                A[y1][x1] = W
+        self.edges = A
+        self.vertices = [i+1 for i in range(V)]
+        self.edgeType = connectionType
     def generateSpanningTree(self):
         #Prims algorithm
         src = self.vertices[0]
--- test.py
+++ test.py
@@ -1,15 +1,6 @@
 from pygraph import Graph
 
-graph = Graph()
-graph.generateAdjacencyMatrix(6)
-graph.addUndirectedEdge(1,5)
-graph.addUndirectedEdge(1,2)
-graph.addUndirectedEdge(2,3)
-graph.addUndirectedEdge(2,5)
-graph.addUndirectedEdge(4,5)
-graph.addUndirectedEdge(4,3)
-graph.addUndirectedEdge(4,6)
-
-graph.generateDegreeMatrix()
-graph.generateLaplacianMatrix()
-print(graph.laplacianMatrix)
+graph = Graph(6)
+graph.generateRandomGraph(False,True,'undirected',False,1,12)
+print(graph.edges)
+print(graph.traverse(1))
